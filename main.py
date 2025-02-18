04c02fd515b6a6858cc5839c76a8666a34402843
Tuguldur0222 <tuuguu.0518@gmail.com>
2025-02-18 16:11:40 +0800

    my first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from functions import minus
+minus_result=minus(5,3)
+print(f"5+3={sum_result}")
+from functions1 import multi
+multi_result=multi(5,3)
+print(f"5*3={multi_result}")
+from f1 import div
+div_result=div(5,3)
+print(f"5/3={div_result}")

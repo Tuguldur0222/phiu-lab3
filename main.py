0169e415982f574d3b57b5bc4b44fa8b55f929ac
Tuguldur0222 <tuuguu.0518@gmail.com>
2025-02-18 16:52:17 +0800

    edit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from functions import minus
+from functions2 import minus
 minus_result=minus(5,3)
-print(f"5+3={sum_result}")
-from functions1 import multi
-multi_result=multi(5,3)
+print(f"5-3={minus_result}")
+from functionshaha import multiple
+multi_result=multiple(5,3)
 print(f"5*3={multi_result}")
 from f1 import div
 div_result=div(5,3)
